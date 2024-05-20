68d6e558187edd2acdc945d7a7340df44973c771
hydrogen <ivan.karpov.88@yandex.ru>
2024-05-20 13:26:20 +0700

    db data

--- src/database.py
+++ src/database.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, declarative_base
+from config import settings
 
-DATABASE_URL = "postgresql://postgres:1337@localhost:5432/admin"
+DATABASE_URL = f"postgresql://{settings.postgres.user}:{settings.postgres.password}@{settings.postgres.host}:{settings.postgres.port}/{settings.postgres.db}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 metadata = MetaData()
-
 
 Base = declarative_base()
 
